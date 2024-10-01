9c90be63775da2d8380b45c2699c07aeb9451d05
RinNguyen2002 <tuanngocnguyenhuu2002@gmail.com>
2024-10-01 22:43:33 +0700

    Update

--- Lab3/mysite/myapp/urls.py
+++ Lab3/mysite/myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 from .views import story_list_view
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('stories/', story_list_view, name='story_list'),
+    path('', views.index, name='index'),  # Trang chính
+    path('stories/', story_list_view, name='story_list'),  # Danh sách câu chuyện
 ]
